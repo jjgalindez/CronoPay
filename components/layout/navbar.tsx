ed7518393d7e2c9ab7dbc0aaf6a360303e452e68
Lenkid <kecci.d.v@outlook.com>
2025-10-28 10:47:17 -0500

    Font Logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar({ variant = 'landing', fixed = true }: NavbarProp
             className="w-8 h-8 md:w-10 md:h-10"
           />
           <span className="text-2xl font-bold text-blue-500">
-            Cronopay
+            𝙲𝚛𝚘𝚗𝚘𝙿𝚊𝚢
           </span>
         </Link>
 
