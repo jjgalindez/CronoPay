755e5490e6bb02bc7e9053155b9cb6865f063f44
JhonGalindez <jhongalindez3@gmail.com>
2025-10-27 21:23:59 -0500

    Delete in sign-up

--- components/ioa.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ioa = () => {
-  return (
-    <div>ioa</div>
-  )
-}
--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -62,7 +62,6 @@ export function SignUpForm({
   };
 
   
-
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
