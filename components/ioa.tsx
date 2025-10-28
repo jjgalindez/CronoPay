f7bf8fa83e23de004a273a24193fa71fd0c6ba06
JhonGalindez <jhongalindez3@gmail.com>
2025-10-27 21:21:55 -0500

    Delete in sign

---
+++ components/ioa.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ioa = () => {
+  return (
+    <div>ioa</div>
+  )
+}
--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -60,7 +60,8 @@ export function SignUpForm({
       setIsLoading(false);
     }
   };
- 
+
+  
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
